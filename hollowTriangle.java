32b47bd9b8a26b4f1ef8fcc47ec7c3fa4c3a4adf
harmonyehe <arini200424@gmail.com>
2024-11-26 22:41:57 +0530

    adding a file

---
+++ hollowTriangle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class hollowTriangle {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if(i==1||i==n||j==1||j==n||i==j){
+                System.out.print(" * ");
+                }else{
+                    System.err.print("   ");
+                }
+            }
+            System.out.println();
+        }
+
+
+    }
+    
+}
